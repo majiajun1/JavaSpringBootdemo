e795ef8e448e0cde23b3a3fc3b2f5a37864c83d4
MaJiaJun1212 <1486902669@qq.com>
2025-01-08 21:30:10 +0800

    commit:FirstSpringBoot202501082130

---
+++ src/test/java/com/example/javaspringbootdemo/JavaSpringBootdemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.javaspringbootdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaSpringBootdemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
